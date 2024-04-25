82e38d9547675e19ea4f5288b89da8815d89f016
王伟 <wangwei@ms-battery.cn>
2024-04-25 18:04:51 +0800

    feat 打包优化

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import dts from 'vite-plugin-dts';
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import terser from '@rollup/plugin-terser'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -37,14 +38,21 @@ export default defineConfig({
       name: 'Vue3ComponentTemplate', // 暴露的全局变量
       fileName: 'index' // 输出的包文件名，默认是 package.json 的 name 选项
     },
-    rollupOptions: {
+    rollupOptions: { // 自定义底层的Rollup打包配置
+      plugins: [
+        terser()
+      ],
       external: ['vue', 'element-plus'],
       output: {
+        name: 'Vue3ComponentTemplate',
+        exports: 'named',
+        externalLiveBindings: false,
         globals: {
           vue: 'Vue',
           "element-plus": "ElementPlus"
         }
       }
-    }
+    },
+    reportCompressedSize: false,
   }
-});
+})
