7e930a946cf16d049f9442716ced3e32f808c059
王伟 <wangwei@ms-battery.cn>
2024-04-26 11:25:02 +0800

    feat 代码优化

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,17 @@
 import DefaultTheme from 'vitepress/theme'
 import './global.scss' 
 
-import Vue3ComponentTemplate from '../../../dist'
-import '../../../dist/style.css'
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
+
+import Vue3ComponentTemplate from '../../../packages'
+import '../../../packages/scss/global.scss'
+
 
 export default {
   extends: DefaultTheme, // or ...DefaultTheme
   enhanceApp ({ app }) {
+    // app.use(ElementPlus)
     app.use(Vue3ComponentTemplate)
   }
 }
