0fba635496c9a10713855adf0b41557fd499c632
王伟 <wangwei@ms-battery.cn>
2024-04-26 10:18:52 +0800

    feat vueuse demo

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,49 @@
-<script setup lang="ts">
-import { ref, computed } from 'vue'
-import { useRoute } from 'vue-router'
-const theme = ref({
-  token: {
-    colorPrimary: '#00b96b'
-  }
-})
-const route = useRoute()
-const routeName = computed(() => {
-  return route.name
-})
-</script>
 <template>
-  <h3>{{ routeName  }}</h3>
+  <div class="app-item">
+    <input v-model="name" placeholder="Enter your name" />
+    <button @click="saveName">Save</button>
+  </div>
+  <div class="app-item">
+    <label>Dark mode:</label>
+    <input type="checkbox" v-model="isDark">
+    <div :class="{ dark: isDark }">
+      Content goes here...
+    </div>
+  </div>
+  <div class="app-item">
+    <div v-if="isSupported">
+      <p>
+        Current copied: <code>{{ text || 'none' }}</code>
+      </p>
+      <input v-model="input" type="text">
+      <button @click="copy(input)">
+        Copy
+      </button>
+    </div>
+    <p v-else>
+      Your browser does not support Clipboard API
+    </p>
+  </div>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useLocalStorage, useDark, useClipboard } from '@vueuse/core';
+
+const name: any = useLocalStorage('name', '');
+
+const saveName = (name) => {
+  // 保存名字到本地存储
+  name.value = name;
+};
+const isDark = useDark();
+
+const input = ref('')
+
+const { text, isSupported, copy } = useClipboard()
+
+</script>
+<style lang="scss" scoped>
+  .app-item {
+    margin-bottom: 20px;
+  }
+</style>
