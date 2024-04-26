95d4b5aba14a92fd02e38161aa863b10d6cfba68
王伟 <wangwei@ms-battery.cn>
2024-04-26 17:33:10 +0800

    feat: update

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,21 +4,21 @@
 set -e
 
 # 读取package.json中的version
-version=`jq -r .version package.json`
-echo "发布前版本：$version"
+# version=`jq -r .version package.json`
+# echo "发布前版本：$version"
 
 # 打包构建
 pnpm build
 
 # 升级 vue-amazing-ui 依赖版本
-pnpm up vue3-component-template@$version
+# pnpm up vue3-component-template@$version
 
 # 提交版本更新代码到github
 git add .
-git cm -m "feat: update $version "
+git commit -m "feat: update"
 git push
 
-echo "当前版本：$version"
+# echo "当前版本：$version"
 
 # 发布到npm，pnpm(高性能的npm)
 pnpm publish --registry http://192.168.3.105:15001/repository/npm-common/
