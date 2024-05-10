0fa44ba68ab20101431beafb1cdb091121eb52e3
王伟 <wangwei@ms-battery.cn>
2024-05-10 14:42:06 +0800

    feat 打包注释

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -12,21 +12,27 @@ echo "执行脚本：$0"
 echo "commit信息：$1"
 
 # 打包构建
+echo "----- npm run build -----"
 npm run build
 
 # 升级 ms-bpmn-js 依赖版本
 # pnpm up ms-bpmn-js@$version
 
 # 提交版本更新代码到github
+echo "----- git add . -----"
 git add .
 
+echo "----- git commit -m "$1" -----"
 git commit -m "$1"
 
+echo "----- git pull -----"
 git pull
 
+echo "----- git push -----"
 git push
 
 # echo "当前版本：$version"
 
 # 发布到npm，pnpm(高性能的npm)
+echo "----- npm publish --registry http://192.168.3.105:15001/repository/npm-common/ -----"
 npm publish --registry http://192.168.3.105:15001/repository/npm-common/
