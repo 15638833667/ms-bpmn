416f29b40bb590d2597ebb5269a23db9f26172e7
王伟 <wangwei@ms-battery.cn>
2024-05-13 09:58:25 +0800

    feat 打包配置优化

--- vite.config.ts
+++ vite.config.ts
@@ -64,15 +64,15 @@ export default defineConfig({
         globals: {
           vue: 'Vue',
           "element-plus": "ElementPlus"
-        },
-        chunkFileNames: 'js/[name]-[hash].js',  // 引入文件名的名称
-        entryFileNames: '[name].js',  // 包的入口文件名称
+        // },
+        // chunkFileNames: 'js/[name]-[hash].js',  // 引入文件名的名称
+        // entryFileNames: '[name].js',  // 包的入口文件名称
         // assetFileNames: '[ext]/[name]-[hash].[ext]', // 资源文件像 字体，图片等
-        inlineDynamicImports: false,
-        manualChunks (id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
+        // inlineDynamicImports: false,
+        // manualChunks (id) {
+        //   if (id.includes('node_modules')) {
+        //     return id.toString().split('node_modules/')[1].split('/')[0].toString();
+        //   }
         }
       }
     },
