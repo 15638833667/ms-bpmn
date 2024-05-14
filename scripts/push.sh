23c3b449827e50aa8b48252bce938dfced20c3f2
王伟 <wangwei@ms-battery.cn>
2024-05-14 18:05:51 +0800

    feat 工具栏配置优化

--- scripts/push.sh
+++ scripts/push.sh
@@ -5,9 +5,21 @@ set -e
 
 # 提交修改到github
 
-git add .
-git commit -m 'feat: update'
+echo "执行脚本：$0"
+echo "commit信息：$1"
 
+# 升级 ms-bpmn-js 依赖版本
+# pnpm up ms-bpmn-js@$version
+
+# 提交版本更新代码到github
+echo "----- git add . -----"
+git add .
+
+echo "----- git commit -m "$1" -----"
+git commit -m "$1"
+
+echo "----- git pull -----"
 git pull
 
+echo "----- git push -----"
 git push
